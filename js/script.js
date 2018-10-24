65f100e948885d1ba6e22eb8b6bbc72a71148b9b
Nicole Saunders <nicostar26@yahoo.com>
2018-10-23 20:02:26 -0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+document.getElementById('btn').addEventListener("click", changeColor);
+
+function changeColor(){
+  thisdiv.style.backgroundColor="orange";
+}
+
+
+document.getElementById('btn1').addEventListener("click", changeColor1);
+
+function changeColor1(){
+  thisdiv.style.backgroundColor="blue";
+}
+
+
+document.getElementById('btn2').addEventListener("click", changeColor2);
+
+function changeColor2(){
+  thisdiv.style.backgroundColor="red";
+}
+
+document.getElementById('btn3').addEventListener("click", changeColor3);
+
+function changeColor3(){
+  thisdiv.style.backgroundColor="white";
+}
